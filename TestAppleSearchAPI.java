b78c4ab2c0d36afce3792fdc28752fce9bfb4f76
vsilverman <vsilverman@yahoo.com>
2017-01-23 10:13:28 -0800

    updated the current output

--- TestAppleSearchAPI.java
+++ TestAppleSearchAPI.java
@@ -21,10 +21,12 @@ public class TestAppleSearchAPI {
 	public static String CURL = "c:\\Program Files\\Git\\mingw64\\bin\\curl";
 	public static String INPUT_DIR_NAME ="d:\\workspace-eclipse-4.5\\AppleTests\\input\\";
 	public static String OUTPUT_DIR_NAME ="d:\\workspace-eclipse-4.5\\AppleTests\\output\\";
+	public static String EXPECTED_OUT = "d:\\workspace-eclipse-4.5\\AppleTests\\current-output\\" + "expected.out";
 	public static File INDIR = new File(INPUT_DIR_NAME);
 	public static File OUTDIR = new File(OUTPUT_DIR_NAME);
 	public static String[] INFILES;
 	public static String[] OUTFILES;
+	public static String[] URL = new String[9];
 
 
 	@BeforeClass
@@ -32,6 +34,15 @@ public class TestAppleSearchAPI {
 		 sapi = new AppleSearchAPI();
 		 INFILES = getDirFiles(INDIR);
 		 OUTFILES = getDirFiles(OUTDIR);
+		 URL[0] = "https://itunes.apple.com/search?term=jack+jackson";
+		 URL[1] = "https://itunes.apple.com/search?term=jack";
+		 URL[2] = "https://itunes.apple.com/search?term=jack+jackson&limit=25";
+		 URL[3] = "https://itunes.apple.com/search?term=jack+jackson&media=musicVideo";
+		 URL[4] = "https://itunes.apple.com/search?term=jim+jones&country=US";
+		 URL[5] = "https://itunes.apple.com/search?term=jim+jones&country=US&media=all";
+		 URL[6] = "https://itunes.apple.com/search?term=jim+jones&country=ca";
+		 URL[7] = "https://itunes.apple.com/search?term=jim+jones&country=US&media=musicVideo";
+		 URL[8] = "https://itunes.apple.com/search?term=yelp&country=US&media=software";
 	}
 
 	@AfterClass
@@ -47,81 +58,63 @@ public class TestAppleSearchAPI {
 	}
 
 	
+	public void test(int i) {
+		System.out.println(INFILES[i]);
+		System.out.println(OUTFILES[i]);
+		String cmd = CURL+" -o "+EXPECTED_OUT+" "+URL[i];
+		sapi.exec(cmd);
+		File expected = new File(EXPECTED_OUT);
+		File actual = new File(OUTPUT_DIR_NAME + OUTFILES[i]);
+		assertEquals(expected.length() , actual.length());
+		assertNotNull(INFILES[i]);
+		assertNotNull(OUTFILES[i]);
+	}
+	
 	@Test
 	public void test1() {
-		System.out.println(INFILES[0]);
-		System.out.println(OUTFILES[0]);
-		String cmd = CURL + " https://itunes.apple.com/search?term=jack+jackson";
-		sapi.exec(cmd);
-		assertNotNull(INFILES[0]);
-		assertNotNull(OUTFILES[0]);
+		test(0);
 	}
 	
 	@Test
 	public void test2() {
-		System.out.println(INFILES[1]);
-		System.out.println(OUTFILES[1]);
-		String cmd = CURL + " https://itunes.apple.com/search?term=jack+jackson";
-		sapi.exec(cmd);
-		assertNotNull(INFILES[1]);
-		assertNotNull(OUTFILES[1]);
+		test(1);
 	}
 	
 	@Test
 	public void test3() {
-		String cmd = CURL + " https://itunes.apple.com/search?term=jack+jackson";
-		sapi.exec(cmd);
-		assertNotNull(INFILES[2]);
-		assertNotNull(OUTFILES[2]);
+		test(2);
 	}
 	
 	@Test
 	public void test4() {
-		String cmd = CURL + " https://itunes.apple.com/search?term=jack+jackson";
-		sapi.exec(cmd);
-		assertNotNull(INFILES[3]);
-		assertNotNull(OUTFILES[3]);
+		test(3);
 	}
 	
 	@Test
 	public void test5() {
-		String cmd = CURL + " https://itunes.apple.com/search?term=jack+jackson";
-		sapi.exec(cmd);
-		assertNotNull(INFILES[4]);
-		assertNotNull(OUTFILES[4]);
+		test(4);
 	}
 	
 	@Test
 	public void test6() {
-		String cmd = CURL + " https://itunes.apple.com/search?term=jack+jackson";
-		sapi.exec(cmd);
-		assertNotNull(INFILES[5]);
-		assertNotNull(OUTFILES[5]);
+		test(5);
 	}
 	
 	@Test
 	public void test7() {
-		String cmd = CURL + " https://itunes.apple.com/search?term=jack+jackson";
-		sapi.exec(cmd);
-		assertNotNull(INFILES[6]);
-		assertNotNull(OUTFILES[6]);
+		test(6);
 	}
 	
 	@Test
 	public void test8() {
-		String cmd = CURL + " https://itunes.apple.com/search?term=jack+jackson";
-		sapi.exec(cmd);
-		assertNotNull(INFILES[7]);
-		assertNotNull(OUTFILES[7]);
+		test(7);
 	}
 	
 	@Test
 	public void test9() {
-		String cmd = CURL + " https://itunes.apple.com/search?term=jack+jackson";
-		sapi.exec(cmd);
-		assertNotNull(INFILES[8]);
-		assertNotNull(OUTFILES[8]);
+		test(8);
 	}
+	
 	
 	
 	private static String[] getDirFiles(File dir) {
